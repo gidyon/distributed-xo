05ef6b807371fa1bed1ed95567b88963ab20b602
Gideon Kamau <gideon.ngk@gmail.com>
2019-10-31 08:21:41 +0300

    two servers each serving secure and insecure http traffic

--- main.go
+++ main.go
@@ -7,28 +7,23 @@ import (
 	"github.com/gidyon/micros/pkg/conn"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 )
 
 const (
-	siteCert = "certs/cert.pem"
-	siteKey  = "certs/key.pem"
-	rootDir  = "dist"
+	siteCert   = "certs/cert.pem"
+	siteKey    = "certs/key.pem"
+	staticsDir = "dist"
 )
 
 func main() {
 	var (
 		certFile      = flag.String("cert", siteCert, "Path to public key")
 		keyFile       = flag.String("key", siteKey, "Path to private key")
-		port          = flag.String("port", ":443", "Port to serve files")
-		root          = flag.String("root", rootDir, "Root directory")
 		redisHost     = flag.String("redis-host", "localhost", "Redis host")
 		redisPort     = flag.String("redis-port", "6379", "Redis port")
 		redisUser     = flag.String("redis-user", "root", "Redis host")
 		redisSchema   = flag.String("redis-schema", "game", "Redis schema")
 		redisPassword = flag.String("redis-password", "menevolent", "Redis password")
-		flush         = flag.Bool("flush", false, "Whether to flush all data in redis")
 		env           = flag.Bool("env", false, "Whether to read parameters from env variables")
 	)
 
@@ -37,15 +32,12 @@ func main() {
 	if *env {
 		*certFile = setIfEmpty(os.Getenv("TLS_CERT_FILE"), *certFile)
 		*keyFile = setIfEmpty(os.Getenv("TLS_KEY_FILE"), *keyFile)
-		*port = setIfEmpty(os.Getenv("PORT"), *port)
-		*root = setIfEmpty(os.Getenv("ROOT_DIR"), *root)
 
 		*redisHost = setIfEmpty(os.Getenv("REDIS_HOST"), *redisHost)
 		*redisPort = setIfEmpty(os.Getenv("REDIS_PORT"), *redisPort)
 		*redisUser = setIfEmpty(os.Getenv("REDIS_USER"), *redisUser)
 		*redisSchema = setIfEmpty(os.Getenv("REDIS_SCHEMA"), *redisSchema)
 		*redisPassword = setIfEmpty(os.Getenv("REDIS_PASSWORD"), *redisPassword)
-		*flush, _ = strconv.ParseBool(os.Getenv("FLUSH_REDIS"))
 	}
 
 	// open redis connection
@@ -54,35 +46,41 @@ func main() {
 		Port:    *redisPort,
 	})
 
-	if *flush {
-		logrus.Infoln("flushind database")
-		defer func() {
-			logrus.Errorln(redisClient.FlushAll().Err())
-			logrus.Errorln(redisClient.FlushDB().Err())
-		}()
-	}
-
 	// start game
 	g, err := newGame(redisClient)
 	if err != nil {
 		logrus.Fatalln(err)
 	}
-	http.HandleFunc("/ws", g.PlayerJoin)
-
 	// static file server
 	staticHandler, err := static.NewHandler(&static.ServerOptions{
-		RootDir: *root,
+		RootDir: staticsDir,
 		Index:   "index.html",
 	})
 	if err != nil {
 		logrus.Fatalln(err)
 	}
-	http.Handle("/", staticHandler)
 
-	logrus.Infof("server started on %v\n", *port)
+	server1 := http.Server{
+		Handler: handler(g, staticHandler),
+		Addr:    ":80",
+	}
 
-	*port = ":" + strings.TrimPrefix(*port, ":")
-	logrus.Fatalln(http.ListenAndServeTLS(*port, *certFile, *keyFile, nil))
+	server2 := http.Server{
+		Handler: handler(g, staticHandler),
+		Addr:    ":443",
+	}
+
+	logrus.Infoln("server started on port 80(http) and 443(https)")
+
+	go server1.ListenAndServe()
+	server2.ListenAndServeTLS(*certFile, *keyFile)
+}
+
+func handler(g *game, staticHandler http.Handler) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ws", g.PlayerJoin)
+	mux.Handle("/", staticHandler)
+	return mux
 }
 
 func setIfEmpty(strCurrent, strFinal string) string {
